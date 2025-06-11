bb1f8a4f1211bc78d27a25797f1a4a6cbb5f9d0f
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-11 23:02:00 +0530

    require the some of packages and connected the mongodb

---
+++ configs/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => console.log("mongoDB connected"));
+    await mongoose.connect(`${process.env.MONGO_DB_URL}/Ecommerce`);
+  } catch (err) {
+    console.err(err.message);
+  }
+};
+module.exports = {
+  connectDB,
+};
--- server.js
+++ server.js
@@ -1,16 +1,27 @@
 const cookieParser = require("cookie-parser");
 const express = require("express");
+const cors = require("cors");
 const app = express();
+// dotenv file
+const dotenv = require("dotenv");
+dotenv.config();
+// db.js file
+const { connectDB } = require("./configs/db.js");
+connectDB();
+// define the variable cors, cors is used to connect the frontend to backend
+// allow multiple origins
+const allowedOrigins = ["http://localhost:5173"];
 
 // Middleware configuration
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 app.get("/", (req, res) => {
   res.send("api is working");
 });
 
-const port = 8080;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log("server started on " + port);
 });
