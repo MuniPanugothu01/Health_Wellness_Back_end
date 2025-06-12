7daf7836aad1495e4a2527d3409d177bfb84751b
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-12 09:34:54 +0530

    cloudinary installed and ceated the file beacuse of it uplaod the image

--- models/Product.js
+++ models/Product.js
@@ -1,18 +1,25 @@
 // models/User.js
 const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  cartItems: {
-    type: Map, // Use Map to store dynamic object-like data
-    of: mongoose.Schema.Types.Mixed, // Allows any type of value
-    default: {},
+const productSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    description: { type: Array, required: true },
+    price: { type: Number, required: true },
+    offerPrice: { type: Number, required: true },
+    image: { type: Array, required: true },
+    category: { type: Number, required: true },
+    inSrock: {
+      type: Boolean,
+      of: mongoose.Schema.Types.Mixed,
+      default: true,
+    },
   },
-}); // {minimize:false}
+  { timestamps: true }
+);
 
-const Product = mongoose.model.product || mongoose.model("product", productSchema);
+const Product =
+  mongoose.model.product || mongoose.model("product", productSchema);
 
 module.exports = {
   Product,
