9785bdb80dce623f9b213feba5281b225390a9bf
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-14 07:27:16 +0530

    few changes in addcontroller

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,11 +1,66 @@
+// const { Address } = require("../models/Address.js");
+
+// // Add Address : /api/address/add
+// const addAddress = async (req, res) => {
+//   try {
+//     const { address, userId } = req.body;
+//     await Address.create({ ...address, userId });
+//     res.json({ success: true, message: "address added successfully😊" });
+//   } catch (error) {
+//     console.log(error.message);
+//     res.json({ success: false, message: error.message });
+//   }
+// };
+
+// // get address : /api/address/get
+// const getAddress = async (req, res) => {
+//   try {
+//     const { userId } = req.body;
+//     const addresses = await Address.find({ userId });
+//     res.json({ success: true, addresses });
+//   } catch (error) {
+//     console.log(error.message);
+//     res.json({ success: false, message: error.message });
+//   }
+// };
+
+// module.exports = {
+//   addAddress,
+//   getAddress,
+// };
+
 const { Address } = require("../models/Address.js");
 
 // Add Address : /api/address/add
 const addAddress = async (req, res) => {
   try {
-    const { address, userId } = req.body;
-    await Address.create({ ...address, userId });
-    res.json({ success: true, message: "address added successfully😊" });
+    const {
+      userId,
+      firstName,
+      lastName,
+      email,
+      street,
+      city,
+      state,
+      zipcode,
+      country,
+      phone,
+    } = req.body;
+
+    await Address.create({
+      userId,
+      firstName,
+      lastName,
+      email,
+      street,
+      city,
+      state,
+      zipcode,
+      country,
+      phone,
+    });
+
+    res.json({ success: true, message: "Address added successfully 😊" });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
