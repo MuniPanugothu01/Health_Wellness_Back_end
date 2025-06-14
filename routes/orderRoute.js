e1fc3c2917d09208a75434e509402f4ee4da97f3
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-14 18:27:11 +0530

    order placed successfully

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,23 +1,4 @@
-// const express = require("express");
-// const {authUser} = require("../middlewares/authUser.js");
-// const {authSeller} = require("../middlewares/authSeller.js");
-// const {
-//   placrOrderCOD,
-//   getUserOrders,
-//   getAllOrders,
-// } = require("../controllers/orderController.js");
 
-// const orderRouter = express.Router();
-
-// orderRouter.post("/cod", authUser, placrOrderCOD);
-// orderRouter.get("/user", authUser, getUserOrders);
-// orderRouter.get("/seller", authSeller, getAllOrders);
-
-// console.log("Handlers:", placrOrderCOD, getUserOrders, getAllOrders);
-
-// module.exports = {
-//   orderRouter,
-// };
 const express = require("express");
 const { authUser } = require("../middlewares/authUser.js");
 const { authSeller } = require("../middlewares/authSeller.js");
@@ -31,7 +12,7 @@ const orderRouter = express.Router();
 
 // ✅ Make sure route paths are lowercase
 orderRouter.post("/cod", authUser, placrOrderCOD);
-orderRouter.get("/user", authUser, getUserOrders);
+orderRouter.post("/user", authUser, getUserOrders);
 orderRouter.get("/seller", authSeller, getAllOrders);
 
 // ✅ Remove unnecessary console.log (optional)
