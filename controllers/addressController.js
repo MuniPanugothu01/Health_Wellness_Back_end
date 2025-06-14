11ec79e640416258ff6429248c0865cb9efced25
MuniPanugothu01 <munipanugothu2001@gmail.com>
2025-06-14 09:13:42 +0530

    address component working good and user address can stored in database, latest addres can dispaly in that cart page

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -1,34 +1,3 @@
-// const { Address } = require("../models/Address.js");
-
-// // Add Address : /api/address/add
-// const addAddress = async (req, res) => {
-//   try {
-//     const { address, userId } = req.body;
-//     await Address.create({ ...address, userId });
-//     res.json({ success: true, message: "address added successfully😊" });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.json({ success: false, message: error.message });
-//   }
-// };
-
-// // get address : /api/address/get
-// const getAddress = async (req, res) => {
-//   try {
-//     const { userId } = req.body;
-//     const addresses = await Address.find({ userId });
-//     res.json({ success: true, addresses });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.json({ success: false, message: error.message });
-//   }
-// };
-
-// module.exports = {
-//   addAddress,
-//   getAddress,
-// };
-
 const { Address } = require("../models/Address.js");
 
 // Add Address : /api/address/add
@@ -70,7 +39,7 @@ const addAddress = async (req, res) => {
 // get address : /api/address/get
 const getAddress = async (req, res) => {
   try {
-    const { userId } = req.body;
+    const { userId } = req.query;
     const addresses = await Address.find({ userId });
     res.json({ success: true, addresses });
   } catch (error) {
